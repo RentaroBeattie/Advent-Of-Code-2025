7fb95ca4619d35e3eb386f924753f63171266a56
RentaroBeattie <rentarobeattie@gmail.com>
2025-12-05 01:36:27 +1100

    Updated Comments

--- Day1/main.go
+++ Day1/main.go
@@ -23,9 +23,11 @@ func ClickThrough(n int, dial int, passes int) int {
 	if n > 0 {
 		passes += x
 	} else {
+		//land on 0 edge case
 		if v%100 == 0 {
 			passes++
 		}
+		//anull above if started on 0
 		if dial == 0 {
 			passes--
 		}
@@ -39,10 +41,12 @@ func ClickThrough(n int, dial int, passes int) int {
 }
 
 func main() {
+	//initialise values
 	var password int = 0
 	var password2 int = 0
 	var dial int = 50
 
+	//open file
 	file, err := os.Open("input.txt")
 	if err != nil {
 		panic(err)
@@ -51,9 +55,11 @@ func main() {
 
 	sign, val := 1, 0
 
+	//start scanning the file line by line
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
+		//determine direction
 		if line[0] == 82 {
 			sign = 1
 		} else {
@@ -65,15 +71,16 @@ func main() {
 			panic(err)
 		}
 
+		//part 2
 		password2 = ClickThrough(sign*val, dial, password2)
 
+		//part 1
 		dial = WrapAround(sign*val, dial)
 
 		if dial == 0 {
 			password += 1
 		}
 
-		//fmt.Printf("%v %v , ", sign, dial)
 	}
 
 	fmt.Println(password)
