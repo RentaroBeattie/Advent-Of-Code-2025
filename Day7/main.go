071eb654094e1ac4612802f2669112ffdd8e9b27
RentaroBeattie <rentarobeattie@gmail.com>
2025-12-08 16:50:43 +1100

    Completed Day7

--- Day7/main.go
+++ Day7/main.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -20,6 +21,8 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	first := true
 	var prev []byte
+	//setup for part 2
+	var p2file [][]byte
 	//scan file line by line
 	for scanner.Scan() {
 		//cast to slice of bytes for mutability
@@ -41,6 +44,36 @@ func main() {
 			}
 		}
 		prev = cur
+		//setup for part2
+		p2file = append(p2file, cur)
 	}
 	fmt.Println(total)
+
+	//part 2
+	//a hashmap to memoize all paths already taken
+	calcedPaths := make(map[[2]int]int)
+	total2 := walk(2, strings.Index(string(p2file[0]), "S"), p2file, calcedPaths)
+	fmt.Println(total2)
+}
+
+func walk(row int, col int, lines [][]byte, calcedPaths map[[2]int]int) int {
+	//paths that have already been taken can be returned without recomputing
+	key := [2]int{row, col}
+	val, found := calcedPaths[key]
+	if found {
+		return val
+	}
+
+	ttotal := 1
+	//find splitter
+	for row < len(lines)-1 && lines[row][col] != '^' {
+		row++
+	}
+	//calculate paths from this splitter
+	if row < len(lines) && lines[row][col] == '^' {
+		ttotal = walk(row+2, col-1, lines, calcedPaths) + walk(row+2, col+1, lines, calcedPaths)
+	}
+	//memoize
+	calcedPaths[key] = ttotal
+	return ttotal
 }
